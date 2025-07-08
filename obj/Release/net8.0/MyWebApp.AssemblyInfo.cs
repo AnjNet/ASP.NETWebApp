fea85318aa0fe569f5c206c93bf2a4924b8312fb
AnjNet <anjaninetala64@gmail.com>
2025-07-08 11:44:38 -0400

    Adding Assignment PDF file

--- obj/Release/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MyWebApp.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67ecbfb183c555f87193c6b48ba523fdaf8847a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679e4aa238cdc637e233282dbb9216b0d7f5a83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
