c67ecbfb183c555f87193c6b48ba523fdaf8847a
AnjNet <anjaninetala64@gmail.com>
2025-07-08 11:20:16 -0400

    Fix: Corrected namespace to MyWebApp.Pages

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Optional: code to run when page loads
+        }
+    }
+}
